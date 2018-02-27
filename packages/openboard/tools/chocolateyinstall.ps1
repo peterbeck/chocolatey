978861fdad774b05c2dcf7504f10d5e050021bac
Peter Beck <peter.beck@foo.li>
2018-02-27 11:17:27 +0100

    try/catch entfernt (request choco gep13)

--- packages/openboard/tools/chocolateyinstall.ps1
+++ packages/openboard/tools/chocolateyinstall.ps1
@@ -14,22 +14,6 @@ $checksumType   = 'sha1'
 $checksum64   	= $checksum
 $checksumType64 = $checksumType
 
-try {   
-	#version not in uninstaller entry, no check at the moment...
-    #$app = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-    #                                'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
-    #        -ErrorAction:SilentlyContinue | Where-Object { $_.DisplayName -like $packageSearch }
-	#
-    #if ($app -and ([version]$app.DisplayVersion -ge [version]$version)) {
-    #    Write-Output $(
-    #    'OpenBoard ' + $version + ' or greater is already installed. ' +
-    #    'No need to download and install again. Otherwise uninstall first.'
-    #    )
-    #} else {
-        Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64 `
-                    -checksum $checksum -checksumType $checksumType `
-                    -checksum64 $checksum64 -checksumType64 $checksumType64
-    #}           
-} catch {
-    throw $_.Exception
-}
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64 `
+    -checksum $checksum -checksumType $checksumType `
+    -checksum64 $checksum64 -checksumType64 $checksumType64
